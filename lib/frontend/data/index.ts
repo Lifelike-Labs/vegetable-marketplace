1ea3a70e507d7a705244af0ea45284cfc4c72e22
Adam Cahan <adam.cahan@gmail.com>
2021-10-26 20:52:50 -0500

    Created `createResourceHook`
    We can use this to construct our `useSWR` hooks for basic resource paths
    like /user/:id, or, as used here, /listings/:id. Can also be used for
    nonslug paths like /listings, etc.
    
    First step in building a framework to tie several of these pieces
    together.

--- lib/frontend/data/index.ts
+++ lib/frontend/data/index.ts
@@ -2,16 +2,34 @@ import { Listing } from '.prisma/client'
 import useSWR, { useSWRConfig } from 'swr'
 import { creater, fetcher } from './helpers'
 
-export function useListing(listingId: string|null) {
-  // SWR conditional fetching - see https://swr.vercel.app/docs/conditional-fetching
+// Handles getting a route with a slug - such as /user/:id - or return `null` if slug isn't a string
+// For non-slug routes function can always return the route path as string
+type GetAPIRoute = (slug: string|null|undefined) => string|null
+
+interface ResourceRequest<ResponseType> {
+  // getRoute is to implement SWR conditional fetching - see https://swr.vercel.app/docs/conditional-fetching
   // This allows us to gracefully handle cases in next.js (like clientside 1st render before route exists)
-  // where this code may be called but listingId does not yet exist.
-  const { data, error } = useSWR<Listing, Error>(listingId ? `/api/listings/${listingId}` : null, fetcher)
-  return {
-    data,
-    error,
+  // where this code may be called but route param/slug does not yet exist.
+  getAPIRoute: GetAPIRoute;
+}
+
+function createUseResourceHook<ResponseType>(resource: ResourceRequest<ResponseType>) {
+  return function(slug?: string|null) {
+    // SWR key is same as our Next.js Page API route
+    const key = resource.getAPIRoute(slug)
+    const { data, error } = useSWR<ResponseType, Error>(key, fetcher)
+    return {
+      data,
+      error,
+    }
   }
 }
+
+const ListingResource: ResourceRequest<Listing> = {
+  getAPIRoute: listingId => listingId ? `/api/listings/${listingId}` : null,
+}
+
+export const useListing = createUseResourceHook(ListingResource)
 
 export function useListings() {
   const { data, error } = useSWR<Listing[], Error>(`/api/listings`, fetcher)
