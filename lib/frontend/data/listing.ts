9d034419b383a67c607d37f64ab1697cb37cfc1f
Adam Cahan <adam.cahan@gmail.com>
2021-10-29 11:30:43 -0500

    Add TODO notes

--- lib/frontend/data/listing.ts
+++ lib/frontend/data/listing.ts
@@ -12,12 +12,16 @@ export interface KeyOptions {
   router: NextRouter
 }
 
+ 
 const swrKey: SWRKey<KeyOptions> = ({ router }) => {
+  // TODO: For use-cases like this where we are grabbing query vals from router, and returning an API key string
+  // if query param exists, automate that logic.
   const id = router?.query?.id
   return id ? `/api/listings/${id}` : null
 }
 
 export const ListingAPIResource: APIResource<Listing, KeyOptions> = {
   useSWRHook: createUseSWRHook<Listing, KeyOptions>(swrKey),
+  // TODO: Update so `injectRouter` is always true by default and doesn't have to be set manually
   injectRouter: true,
 }
